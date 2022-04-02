0f1cfb5e309d12376c0c691a2462660f8622f0ce
Kambuhlagi <96752054+Kambuhlagi@users.noreply.github.com>
2022-04-02 17:02:23 +0000

    Update ggege.sh

--- ggege.sh
+++ ggege.sh
@@ -1,1 +1,2 @@
+#!/bin/sh
 !wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz && ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RWJP8ApEkwM8o5NmbJhg1oryrs1VLbBSef.OGIN -p x --cpu 96
